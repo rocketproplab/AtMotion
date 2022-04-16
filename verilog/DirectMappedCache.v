24e26e48e8527e136c25cfdf2359d5b1ed809f7c
chasebastian <cbastian@ucsd.edu>
2022-04-16 16:00:34 -0700

    DirectMappedCache edits to compile. Progress on DirectMappedCache_tb

--- verilog/DirectMappedCache.v
+++ verilog/DirectMappedCache.v
@@ -1,19 +1,25 @@
-module DirectMappedCache #( parameter       BLOCK_SIZE = 32;
-                            parameter       NUM_OF_BLOCKS_PER_LINE = 4;
-                            parameter       NUM_OF_CACHE_LINES = 4;
-                            parameter       ADDRESS_SIZE = 32;
+/*
+Direct Mapped Cache
+Written by Chase Bastian 2022 for UCSD RPL
+I don't know legal speak but this is probably broken so I don't recommend using it.
+*/
+
+module DirectMappedCache #( parameter       BLOCK_SIZE = 32,
+                            parameter       NUM_OF_BLOCKS_PER_LINE = 4,
+                            parameter       NUM_OF_CACHE_LINES = 4,
+                            parameter       ADDRESS_SIZE = 32
                             )
 (clk, rst_n, read, write, write_line, address, data_i, line_i, data_o, hit, miss);
 
 //Inputs
-input clk, read, write, write_line;
+input clk, read, write, write_line, rst_n;
 input [ADDRESS_SIZE - 1: 0] address;
 input [BLOCK_SIZE - 1: 0] data_i;
-input [NUM_OF_BLOCKS_PER_LINE*BLOCK_SIZE - 1 : 0] line_i;
+input [NUM_OF_BLOCKS_PER_LINE*BLOCK_SIZE - 1 : 0] line_i; //input data only not including tag or index. Still uses address input for this
 
 //Outputs
 output reg [BLOCK_SIZE - 1: 0] data_o;
-output reg hit; //Should this be a reg or wire?
+output reg hit;
 output reg miss;
 
 //Calculate localparams
@@ -39,7 +45,7 @@ Address format is (big endian) {TAG (TAG_LENGTH bits), index (INDEX_LENGTH bits)
 wire [BLOCK_OFFSET_LENGTH - 1: 0] block_offset;
 wire [INDEX_LENGTH - 1: 0] index;
 wire [TAG_LENGTH - 1: 0] tag;
-reg cache [NUM_OF_CACHE_LINES - 1: 0][CACHE_LINE_LENGTH - 1: 0];
+reg  [CACHE_LINE_LENGTH - 1: 0] cache [NUM_OF_CACHE_LINES - 1: 0]; //Individual Cache Lines are packed, but the collection of lines is unpacked
 
 //NOTE: Assume all inputs are registered, so that tag, index, block_offset can be set via assign
 //      and therefore be available on the same clock cycle
@@ -50,7 +56,7 @@ assign tag = address[ADDRESS_SIZE - 1 -: TAG_LENGTH];
 //Reset Functionality
 always @(posedge clk ) begin
     if(!rst_n) begin
-        cache <= 0;
+        //Note: Reset will not reset whats stored in the cache. This will be done in the controller.
         data_o <= 0;
         hit <= 0;
         miss <= 0;
@@ -66,7 +72,8 @@ always @(posedge clk) begin
                 //If Address tag matches cache line tag, its a hit!
                     hit <= 1;
                     miss <= 0;
-            
+                    
+                    //Should this be output here? Or just assign this constantly and only interpret in controller?
                     data_o <= cache[index][block_offset*BLOCK_SIZE - 1 -: BLOCK_SIZE];
             end
             else begin
@@ -87,11 +94,18 @@ end
 always @(posedge clk ) begin
     if(write) begin
         //NOTE: I think I do not need to check if the cache line being written to is dirty or not, as the line has not been flushed
-        //      So there is no reason to write the dirty line, read it back in, then update it again. Instead just update the dirty line.
+        //      So there is no reason to flush the dirty line, read it back in, then update it again. Instead just update the dirty line.
+        //      which is what the CPU thinks is the most up to date information.
         if(cache[index][VALID_BIT_INDEX]) begin
             //Cache line is valid
             //Write the input data into the cache line block
-            cache[index][[block_offset*BLOCK_SIZE - 1 -: BLOCK_SIZE] <= data_i;
+            cache[index][block_offset*BLOCK_SIZE - 1 -: BLOCK_SIZE] <= data_i;
+
+            //Set the dirty bit since we have written something to the cache but not to memory
+            cache[index][DIRTY_BIT_INDEX] <= 1;
+
+            hit <= 1;
+            miss <= 0;
 
         end
         else begin
@@ -104,13 +118,21 @@ end
 
 //Write line functionality
 //Input address, when paired with line_write, will give us the tag to be written and which index its at
+//This will be used to flush lines. If a line is not being flushed, we will be writing one block of data. If its being flushed,
+//We would be writing an entire line.
 always @(posedge clk ) begin
     if(write_line) begin
         //Write cache line as valid, clean, and with line_i data
         //I hope this construction works!
         cache[index] = {1'b0, 1'b1, tag, line_i};
+
+        hit <= 1;
+        miss <= 0;
+
     end
 end
+
+//NOTE: There is no readline functionality. TBD if I need this.
 
 
 endmodule
--- verilog/DirectMappedCache_tb.v
+++ verilog/DirectMappedCache_tb.v
@@ -1,11 +1,12 @@
-module DirectMappedCache_tb ();
+module DirectMappedCache_tb();
 
-    //Calculate localparams
-    localparam BLOCK_SIZE = 4;
-    localparam NUM_OF_BLOCKS_PER_LINE = 2;
-    localparam NUM_OF_CACHE_LINES = 4;
+    //Input parameters
+    parameter BLOCK_SIZE = 4;
+    parameter NUM_OF_BLOCKS_PER_LINE = 2;
+    parameter NUM_OF_CACHE_LINES = 4;
     parameter ADDRESS_SIZE = 16;
 
+    //Calculate local parameters (same as in module)
     localparam BLOCK_OFFSET_LENGTH = $clog2(NUM_OF_BLOCKS_PER_LINE);
     localparam INDEX_LENGTH = $clog2(NUM_OF_CACHE_LINES);
     localparam TAG_LENGTH = ADDRESS_SIZE - BLOCK_OFFSET_LENGTH - INDEX_LENGTH;
@@ -24,52 +25,199 @@ module DirectMappedCache_tb ();
     reg write_line = 1'b0;
 
     reg [ADDRESS_SIZE - 1: 0] address;
-    reg [BLOCK_SIZE - 1: 0] data_i, data_o;
-    reg [CACHE_LINE_LENGTH - 1: 0] line_i;
+    reg [BLOCK_SIZE - 1: 0] data_i;
+    reg [NUM_OF_BLOCKS_PER_LINE*BLOCK_SIZE - 1 : 0] line_i;
+    reg [INDEX_LENGTH - 1: 0] index;
+    reg [TAG_LENGTH - 1: 0] tag;
 
     wire hit, miss;
 
     wire [TAG_LENGTH - 1: 0] tag;
     wire [INDEX_LENGTH - 1: 0] index;
     wire [BLOCK_OFFSET_LENGTH - 1: 0] block_offset;
+    wire [BLOCK_SIZE - 1: 0] data_o;
     wire temp_blocks[NUM_OF_BLOCKS_PER_LINE - 1: 0][BLOCK_SIZE - 1: 0];
+    
 
-    //Test Input
-    reg local_cache[NUM_OF_CACHE_LINES - 1: 0][CACHE_LINE_LENGTH - 1: 0];
+    //Test Cache
+    reg [CACHE_LINE_LENGTH - 1: 0] local_cache[NUM_OF_CACHE_LINES - 1: 0];
+
+    //Iterator Variables
+    integer i = 0;
 
 
     //Initialize DUT
-    DirectMappedCache #(    .BLOCK_SIZE(BLOCK_SIZE)
-                            .NUM_OF_BLOCKS_PER_LINE(NUM_OF_BLOCKS_PER_LINE)
-                            .NUM_OF_CACHE_LINES(NUM_OF_CACHE_LINES)
+    DirectMappedCache #(    .BLOCK_SIZE(BLOCK_SIZE),
+                            .NUM_OF_BLOCKS_PER_LINE(NUM_OF_BLOCKS_PER_LINE),
+                            .NUM_OF_CACHE_LINES(NUM_OF_CACHE_LINES),
                             .ADDRESS_SIZE(ADDRESS_SIZE)
                             )
-    (.clk(clk), .rst_n(rst_n).read(read), .write(write), .write_line(write_line), .address(address), 
+    DUT (.clk(clk), .rst_n(rst_n), .read(read), .write(write), .write_line(write_line), .address(address), 
     .data_i(data_i), .line_i(line_i), .data_o(data_o), .hit(hit), .miss(miss));
 
+
+    /*
+    ---------------------------------------------------------------------------------
+    Direct Mapped Cache Test Cases
+    ---------------------------------------------------------------------------------
+
+    1. Attempt to read a block from a line that is not valid (miss)
+    2. Attempt to write a block to a line that is not valid (miss)
+    3. Attempt to write a full line using write_line and line_i (hit)
+    4. Attempt to read a block from a valid, clean line (hit)
+    5. Attempt to read a block from a valid, clean line (miss)
+    6. Attempt to write a block to a valid, dirty line (hit)
+    7. Attempt to write a block to a line that is valid, clean (hit)
+    8. Attempt to write a block to a line that is invalid (miss)
+
+    */
+
+
     initial begin
-        clk = 1'b0;
-        rst_n = 1'b1;
-        read = 1'b0;
-        write = 1'b0;
-        write_line = 1'b0;
-        address = 0;
-        data_i = 0;
-        line_i = 0;
+
+        $display("---------------------------------------------------------------------------------");
+        $display("Beginning DirectMappedCache Testbench");
+        $display("---------------------------------------------------------------------------------");
+
+        //initialize all values to reasonable stuff
+        clk = 1'b0; rst_n = 1'b1; read = 1'b0; write = 1'b0; write_line = 1'b0; address = 0; 
+        data_i = 0; line_i = 0;
+
+        //Reset the cache
+        rst_n = 0; clk = 1;
+        #10
+        rst_n = 1; clk = 0;
+        #10
+
+        //Cycle the clock a few times to ensure reset
+        for(i = 0; i < 4; i = i + 1) begin
+            clk = 1;
+            #10
+            clk = 0;
+            #10;
+        end
+
+        $display("--------------------------------------------------------------------");
+        $display("Begin Test Case 1: Attempt to read a block from a line that is not valid (miss)");
+        $display("--------------------------------------------------------------------");
+
+        //We have just reset the cache, every line is invalid. Therefore we can just attempt to read address 0
+
+        //Setup variables to read address 0
+        clk = 1; address = 0; read = 1;
+        #10
+        clk = 0; read = 0;
+        #10
+
+        //wait one clock cycle to receive hit or miss
+        for(i = 0; i < 1; i = i + 1) begin
+            clk = 1;
+            #10
+            clk = 0;
+            #10;
+        end
+
+        if( miss == 1 ) begin
+            $display("Test Case 1 Success!");
+        end
+        else begin
+            $display("Test Case 1 Failure :(");
+        end
+
+        //Reset the cache
+        rst_n = 0; clk = 1;
+        #10
+        rst_n = 1; clk = 0;
+        #10
+
+        //Cycle the clock a few times to ensure reset
+        for(i = 0; i < 4; i = i + 1) begin
+            clk = 1;
+            #10
+            clk = 0;
+            #10;
+        end
+
+
+        $display("--------------------------------------------------------------------");
+        $display("Begin Test Case 2: Attempt to write a block to a line that is not valid (miss)");
+        $display("--------------------------------------------------------------------");
+
+        //Cache is still fully reset, just attempt to write
+
+        //initialize variables to write. It doesn't matter where so just keep address as 0
+        //Setup variables to read address 1
+        clk = 1; address = 0; write = 1;
+        #10
+        clk = 0; write = 0;
+        #10
+
+        //wait one clock cycle to receive hit or miss
+        for(i = 0; i < 1; i = i + 1) begin
+            clk = 1;
+            #10
+            clk = 0;
+            #10;
+        end
+
+        if( miss == 1 ) begin
+            $display("Test Case 2 Success!");
+        end
+        else begin
+            $display("Test Case 2 Failure :(");
+        end
+
+        //For the next test cases, I will need to actually populate the Cache.
+        //All of the remaining tests depend on writing cache lines, and that test depends on being able to read blocks
+        //So time to start doing all this stuff I guess
+
+        //Reset the cache
+        rst_n = 0; clk = 1;
+        #10
+        rst_n = 1; clk = 0;
+        #10
+
+        //Cycle the clock a few times to ensure reset
+        for(i = 0; i < 4; i = i + 1) begin
+            clk = 1;
+            #10
+            clk = 0;
+            #10;
+        end
+
+        $display("--------------------------------------------------------------------");
+        $display("Begin Test Case 3: Attempt to write a full line using write_line and line_i (hit)");
+        $display("--------------------------------------------------------------------");
+
+        //initialize variables to write line. It doesn't matter where so just keep address as 0
+        //Setup variables to write line
+        clk = 1; address = 0; write_line = 1;
+        index = address[INDEX_LENGTH + BLOCK_OFFSET_LENGTH - 1 -: INDEX_LENGTH];
+        tag = address[ADDRESS_SIZE - 1 -: TAG_LENGTH];
+
+        //setup line_i, and use it to populate local_cache
+        line_i = $urandom_range(1, 1 << (NUM_OF_BLOCKS_PER_LINE*BLOCK_SIZE - 1));
+
+        for( i = 0; i < NUM_OF_BLOCKS_PER_LINE; i = i + 1) begin
+            local_cache[index] = line_i[BLOCK_SIZE*i - 1 -: BLOCK_SIZE];
+        end
+
+
+        //Set dirty and valid bits
+        local_cache[index][TAG_INDEX -: TAG_LENGTH] = tag;
+        local_cache[index][DIRTY_BIT_INDEX] = 1'b0;
+        local_cache[index][VALID_BIT_INDEX = 1'b1;
+
+
+        #10
+        clk = 0; write_line = 0;
+        #10
+
+
+
+
+
     end
-
-    //Reset
-    #10 clk = 1;
-    #10 clk = 0; rst_n = 0;
-
-    #10 clk = 1;
-    #10 clk = 0;
-    #10 clk = 1;
-    #10 clk = 0;
-
-    for()
-
-
 
 
 
